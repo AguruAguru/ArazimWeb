701d79a8ed4562e99c7b4681023cf7b45f411b89
Dolev <oz.dolev1@gmail.com>
2023-06-06 19:56:36 +0300

    login

--- flask1.py
+++ flask1.py
@@ -1,13 +1,37 @@
 import os
+
+import flask
 from flask import Flask, render_template, request
 import json
 
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    info = request.cookies.get('info')
+    return render_template("index.html", info="" if info is None else "Hello " + info + "!")
+
+@app.route('/login')
+def login():
+    resp = flask.make_response(render_template("login.html"))
+    name = request.args.get("name")
+    psw = request.args.get("psw")
+    err = None
+    if None not in [name, psw]:
+        with open('data.json', 'r+') as f:
+            data = json.load(f)
+            if name in data.keys() and data[name]==psw:
+                resp.set_cookie('info', request.args.get("name"))
+            else:
+                err = "incorrect details"
+    if err is not None:
+        resp = flask.make_response(render_template("login.html", err=err))
+    return resp
+
 
 @app.route('/signup')
-def homepage():
+def signup():
     err = None
     name = request.args.get("name")
     psw = request.args.get("psw")
@@ -25,7 +49,7 @@ def homepage():
             f.seek(0)  # <--- should reset file position to the beginning.
             json.dump(data, f, indent=4)
             f.truncate()
-    return render_template('main.html', err="" if err is None else err)
+    return render_template('signup.html', err="" if err is None else err)
 
 if __name__ == '__main__':
     app.run(host='localhost', port=5000)
