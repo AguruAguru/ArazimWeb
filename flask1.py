0d133ae52f4f7d06416973687a8b578a508aece2
Dolev <oz.dolev1@gmail.com>
2023-06-06 18:46:51 +0300

    flask2

--- flask1.py
+++ flask1.py
@@ -4,8 +4,8 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 @app.route('/')
-def homepage():
-    return 'hello world'
+def home():
+   return render_template('main.html')
 
 if __name__ == '__main__':
     app.run(host='localhost', port=5000)
