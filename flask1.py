71c7664e4a1f3642bf9dffa0d9d163c39d59e2f9
Dolev <oz.dolev1@gmail.com>
2023-06-06 19:40:34 +0300

    signup

--- flask1.py
+++ flask1.py
@@ -1,15 +1,32 @@
 import os
 from flask import Flask, render_template, request
-from jinja2 import Environment, FileSystemLoader
+import json
+
 
 app = Flask(__name__)
-environment = Environment(loader=FileSystemLoader("templates/"))
-template = environment.get_template("main.html")
 
-@app.route('/')
-def home():
-   return template.render(name=request.args.get('name'))
 
+@app.route('/signup')
+def homepage():
+    err = None
+    name = request.args.get("name")
+    psw = request.args.get("psw")
+    if None not in [name, psw]:
+        with open('data.json', 'r+') as f:
+            data = json.load(f)
+            if len(psw) < 4:
+                err = "Password too small"
+            if name in data:
+                err = "Name taken"
+            if name == "":
+                err = "name cant be empty"
+            if err is None:
+                data[name] = psw
+            f.seek(0)  # <--- should reset file position to the beginning.
+            json.dump(data, f, indent=4)
+            f.truncate()
+    return render_template('main.html', err="" if err is None else err)
 
 if __name__ == '__main__':
     app.run(host='localhost', port=5000)
+
